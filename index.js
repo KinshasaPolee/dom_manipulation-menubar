c4a82e03308cc60597caa40f575519ac3ee2867d
KinshasaPolee <kpolee1@gmail.com>
2023-12-07 01:22:46 -0500

    pt.2 step 2/3

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
 mainEl.classList.add('flex-ctr');
 
 const topMenuEl = document.getElementById('top-menu');
-
+topMenuEl.style.height = '100%';
+var topMenuBgColor = getComputedStyle(document.documentElement).getPropertyValue('--top-menu-bg');
+topMenuBgColor.style.backgroundColor = topMenuBgColor;
