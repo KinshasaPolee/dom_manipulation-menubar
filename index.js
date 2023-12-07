900076220713b79278b18ecddcbdb843118217af
KinshasaPolee <kpolee1@gmail.com>
2023-12-07 01:01:08 -0500

    step 3 complete

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import "./styles.css";
 const mainEl = document.querySelector('main');
 var mainBgColor = getComputedStyle(document.documentElement).getPropertyValue('main-bg');
 mainEl.style.backgroundColor = 'var(' + mainBgColor + ')';
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
