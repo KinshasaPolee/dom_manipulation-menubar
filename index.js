3f8db41c632bd0e395417d6dd6965d06dcadd82c
KinshasaPolee <kpolee1@gmail.com>
2023-12-07 00:57:45 -0500

    step 2 complete

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 import "./styles.css";
 const mainEl = document.querySelector('main');
+var mainBgColor = getComputedStyle(document.documentElement).getPropertyValue('main-bg');
+mainEl.style.backgroundColor = 'var(' + mainBgColor + ')';
