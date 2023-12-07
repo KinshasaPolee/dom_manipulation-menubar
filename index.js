63ef9d7b7834bfd32d0af6ad8e9831035a786629
KinshasaPolee <kpolee1@gmail.com>
2023-12-07 15:11:22 -0500

    updated files

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var menuLinks = [
     { text: 'account', href: '/account' },
 ];
 
-import "./styles.css";
+// import "./styles.css";
 const mainEl = document.querySelector('main');
 var mainBgColor = getComputedStyle(document.documentElement).getPropertyValue('main-bg');
 mainEl.style.backgroundColor = 'var(' + mainBgColor + ')';
