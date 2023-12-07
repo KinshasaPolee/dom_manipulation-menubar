7c09f3546e4830c5d263fa414c45d51271edf3f1
KinshasaPolee <kpolee1@gmail.com>
2023-12-07 01:10:06 -0500

    step 4 complete

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const mainEl = document.querySelector('main');
 var mainBgColor = getComputedStyle(document.documentElement).getPropertyValue('main-bg');
 mainEl.style.backgroundColor = 'var(' + mainBgColor + ')';
 mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+mainEl.classList.add('flex-ctr');
